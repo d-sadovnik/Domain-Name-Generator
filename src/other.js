0167e87fdf81c1c433312927721908d5d9f82d41
d-sadovnik <borixe@gmail.com>
2022-08-25 03:28:53 +0000

    please commit

--- src/other.js
+++ src/other.js
@@ -1,18 +1,10 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = () => {
-  document.querySelector("#btn").addEventListener("click", () => {
-    document.querySelector("#misdominios").innerHTML = domainGenerate();
-  });
-};
+    document.querySelector("#btn").addEventListener("click", () => {
+      document.querySelector("#misdominios").innerHTML = domainGenerate();
+    });
+  };
 
-let domainGenerate = () => {
-  let pronouns = ["the", "our"];
+let pronouns = ["the", "our"];
   let adjs = ["great", "big"];
   let nouns = ["jogger", "racoon"];
   let dots = [".com", ".us", ".net", ".io"];
@@ -26,4 +18,3 @@ let domainGenerate = () => {
       }
     }
   }
-};
