8182a08ed2b4301bafb1fca12167bf9f34c2cc87
d-sadovnik <borixe@gmail.com>
2022-08-25 16:44:05 +0000

    new .js file

---
+++ src/other.js
@@ -0,0 +1,19 @@
+let domainGenerate = () => {
+    let pronouns = ["the", "our"];
+    let adjs = ["great", "big"];
+    let nouns = ["jogger", "racoon"];
+    let dots = [".com", ".us", ".net", ".io"];
+  
+    for (let i = 0; i < pronouns.length; i++) {
+      for (let j = 0; j < adjs.length; j++) {
+        for (let k = 0; k < nouns.length; k++) {
+          for (let l = 0; l < dots.length; l++) {
+            console.log(pronouns[i] + adjs[j] + nouns[k] + dots[l]);
+          }
+        }
+      }
+    }
+  };
+  
+  domainGenerate();
+  
